1410002776d7d650e55fb9b2150deb9ae90b9cbf
Rebecca Adeyeye <adeyeyebecky@gmail.com>
2020-03-13 16:24:06 +0100

    Solved the tasks on array

---
+++ Arrays/Arrays.js
@@ -0,0 +1,39 @@
+/*
+Array is a type of data structure. It is used for storing data and can contain or store a large number of data that has a similar data type.
+
+Each item in an array is called an element.
+The total number of elements in an array is called length. Counting an array starts from zero.
+Each element also has its own index, which is used to access the element
+There are different operations that can be performed when using an array. They include; 
+
+Traverse − print all the array elements one by one.
+Insertion − Adds an element at the given index.
+Deletion − Deletes an element at the given index.
+Search − Searches an element using the given index or by the value.
+Update − Updates an element at the given index.
+*/
+
+// To create an empty array, the syntax below is used;
+
+let arr = [];
+
+// Array Task 2
+let colors = ['blue', 'green']
+
+// append color indigo to the end
+colors.push("indigo");
+console.log(colors);
+
+// Replace the middle value with "red"
+colors.splice(1,1,"red")
+console.log(colors)
+
+// Strip off the first value of the array
+colors.shift();
+console.log(colors);
+
+// Prepend gold and orange to the array.
+colors.unshift("gold", "orange")
+console.log(colors);
+
+
---
+++ Arrays/numbers-filtering.js
@@ -0,0 +1,50 @@
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+let evenNumbers = numbers.filter(e => e % 2 === 0
+
+)
+ console.log(evenNumbers);
+
+
+
+ //4. Give the below array of object:
+
+ const userScore = [  
+    {id: 1, name: 'Olarike', score: 34},  
+    {id: 2, name: 'Taiwo', score: 12},  
+    {id: 3, name: 'Kehinde', score: 27} ];
+
+
+// Find where user score above 12 and return the user info.
+
+let aboveTwelve = userScore.filter(e => e.score > 12)
+console.log(aboveTwelve)
+
+// Sort the array in descending order based on the user score.
+ userScore.sort(function (a,b) {
+     return b.score - a.score
+ })
+
+console.log(userScore)
+
+// Loop through the array after sort and return the each user. Note your position should start at 1.
+
+for (index = 1; index < userScore.length; index++) { 
+    console.log(userScore[index]); 
+} 
+
+// Sum the total score of the user in the array.
+
+let total = userScore.map(i => i.score).reduce((prev, next) => prev + next);
+
+console.log("The sum of all scores is " + total)
+
+// Merge the below array to the userScore array above:
+
+userScore.push(
+    {id: 5, name: 'wale', score: 34},  
+    {id: 6, name: 'wole', score: 12},  
+    {id: 4, name: 'bobby', score: 27}
+)
+
+console.log(userScore)
